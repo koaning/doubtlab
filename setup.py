fc09aef38b922388a85753ba7c6c83f8e3f02eb2
vincent d warmerdam <vincentwarmerdam@gmail.com>
2021-11-14 21:40:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     },
     install_requires=base_packages,
     extras_require={"dev": dev_packages},
+    license_files = ('LICENSE',)
     classifiers=[
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
