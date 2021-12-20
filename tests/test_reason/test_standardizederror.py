a1bded62e6f960140624782e0fc83849c95717ad
FBruzzesi <fbruzzesi@libero.it>
2021-12-20 18:53:51 +0100

    split StandardizedErrorReason test into two

--- tests/test_reason/test_standardizederror.py
+++ tests/test_reason/test_standardizederror.py
@@ -13,6 +13,9 @@ def test_from_predict():
         predicate == np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
     )
 
+
+def test_from_predict_no_reason():
+    """Test `from_predict` on an obvious examples"""
     y = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
     preds = y + np.random.choice([-0.05, 0, 0.05], 10)
     predicate = StandardizedErrorReason.from_predict(pred=preds, y=y, threshold=3.0)
