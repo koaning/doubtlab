b3c57015f892c90460fd82f4ea888bda190ed2b6
Vincent <vincentwarmerdam@gmail.com>
2021-12-27 21:44:44 +0100

    added-plotting-tools

--- doubtlab/benchmark.py
+++ doubtlab/benchmark.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def _parse_check_p_n_y(p, n, y):
@@ -92,3 +93,94 @@ def flip_labels(y, random_seed=42, n=None, p=None):
     idx = np.random.choice(np.arange(y.shape[0]), size=n, replace=False)
     y_out[idx] = [np.random.choice(classes[classes != _]) for _ in y_out[idx]]
     return y_out, (y != y_out).astype(int)
+
+
+def calculate_precision_recall_at_k(
+    predicate_df, idx_flip, max_k=100, give_random=False, give_ensemble=True
+):
+    """
+    Plots precision/recall at `k` values for flipped label experiments.
+
+    Returns an interactive altair visualisation. Make sure it is installed beforehand.
+
+    Arguments:
+        - origin_df: the original dataframe that you start with
+        - predicate_df: the dataframe with predicates from `ensemble.get_predicates`
+        - max_k: the maximum value for `k` to consider
+        - give_random: plot the "at k" statistics for the randomly selected lower bound
+        - give_ensemble: plot the "at k" statistics from the reason ensemble
+    """
+    # First we need to ensure that the original dataframe with X values is
+    # is combined with our reasons dataframe and sorted appropriately.
+    df = predicate_df.assign(
+        s=lambda d: d[[c for c in d.columns if "predicate" in c]].sum(axis=1),
+        flipped=idx_flip,
+    ).sort_values("s", ascending=False)
+
+    # Next we calculate the precision/recall at k values
+    data = []
+    for k in range(1, max_k):
+        recall_at_k = df["flipped"][:k].sum() / df["flipped"].sum()
+        precision_at_k = (df["flipped"][:k] == np.ones(k)).sum() / k
+        random_recall = df["flipped"].mean() * k / df["flipped"].sum()
+        random_precision = df["flipped"].mean()
+        data.append(
+            {
+                "recall_at_k": recall_at_k,
+                "precision_at_k": precision_at_k,
+                "k": k,
+                "setting": "ensemble",
+            }
+        )
+        data.append(
+            {
+                "recall_at_k": random_recall,
+                "precision_at_k": random_precision,
+                "k": k,
+                "setting": "random",
+            }
+        )
+    result = pd.DataFrame(data).melt(["k", "setting"])
+    # Give the user the option to only return draw a subset
+    if not give_random:
+        result = result.loc[lambda d: d["setting"] != "random"]
+    if not give_ensemble:
+        result = result.loc[lambda d: d["setting"] != "ensemble"]
+
+    # Return the data in a tidy format.
+    return result
+
+
+def plot_precision_recall_at_k(
+    predicate_df, idx_flip, max_k=100, give_random=True, give_ensemble=True
+):
+    """
+    Plots precision/recall at `k` values for flipped label experiments.
+
+    Returns an interactive altair visualisation. Make sure it is installed beforehand.
+
+    Arguments:
+        - origin_df: the original dataframe that you start with
+        - predicate_df: the dataframe with predicates from `ensemble.get_predicates`
+        - max_k: the maximum value for `k` to consider
+        - give_random: plot the "at k" statistics for the randomly selected lower bound
+        - give_ensemble: plot the "at k" statistics from the reason ensemble
+    """
+    import altair as alt
+
+    # We combine the results in dataframes
+    plot_df = calculate_precision_recall_at_k(
+        predicate_df=predicate_df,
+        idx_flip=idx_flip,
+        max_k=max_k,
+        give_random=give_random,
+        give_ensemble=give_ensemble,
+    )
+
+    # So that we may plot it.
+    return (
+        alt.Chart(plot_df)
+        .mark_line()
+        .encode(x="k", y="value", color="variable", strokeDash="setting")
+        .interactive()
+    )
