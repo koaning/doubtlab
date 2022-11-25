cedc6385e160d1e8c2a6acd4de3e3beef8ca983b
Vincent <vincentwarmerdam@gmail.com>
2022-11-25 12:19:31 +0100

    added

--- tests/test_reason/test_wrongpred.py
+++ tests/test_reason/test_wrongpred.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from doubtlab.reason import WrongPredictionReason
 
@@ -8,3 +9,36 @@ def test_from_predict():
     y = np.array(["positive", "neutral"])
     predicate = WrongPredictionReason.from_predict(pred=preds, y=y)
     assert np.all(predicate == np.array([0.0, 1.0]))
+
+
+def test_from_predict_fp():
+    """Test `from_predict` on an obvious fp example"""
+    preds = np.array([0, 0, 1, 0])
+    y = np.array([0, 0, 1, 1])
+    predicate = WrongPredictionReason.from_predict(pred=preds, y=y, method="fp")
+    assert np.all(predicate == np.array([0.0, 0.0, 0.0, 1.0]))
+
+
+def test_from_predict_fn():
+    """Test `from_predict` on an obvious fn example"""
+    preds = np.array([0, 0, 1, 1])
+    y = np.array([0, 0, 1, 0])
+    predicate = WrongPredictionReason.from_predict(pred=preds, y=y, method="fn")
+    assert np.all(predicate == np.array([0.0, 0.0, 0.0, 1.0]))
+
+
+def test_value_error():
+    """Test `from_predict` on an obvious value-error example"""
+    preds = np.array([0, 1, 2])
+    y = np.array([0, 1, 0])
+    with pytest.raises(ValueError):
+        WrongPredictionReason.from_predict(pred=preds, y=y, method="fn")
+    with pytest.raises(ValueError):
+        WrongPredictionReason.from_predict(pred=preds, y=y, method="fp")
+
+    preds = np.array([0, 1, 0])
+    y = np.array([0, 1, 2])
+    with pytest.raises(ValueError):
+        WrongPredictionReason.from_predict(pred=preds, y=y, method="fn")
+    with pytest.raises(ValueError):
+        WrongPredictionReason.from_predict(pred=preds, y=y, method="fp")
